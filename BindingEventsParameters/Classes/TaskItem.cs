ed2997a3903ea284def8165bb3f89cdf9191042c
Iqra <247274@students.au.edu.pk>
2026-05-23 06:42:18 -0700

    Add project files.

---
+++ BindingEventsParameters/Classes/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace BindingEventsParameters.Classes
+{
+    public class TaskItem
+    {
+        
+            public string TaskDescription { get; set; }
+        
+            public bool IsComplete { get; set; }
+       
+}
+}
+
